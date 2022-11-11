47a2dccd1d233517f2a86df964f32fbcb9b554df
Vladislav <74150372+elec7reify@users.noreply.github.com>
2022-11-11 17:26:04 +0100

    Create ChatUtil.java

---
+++ src/main/java/com/flexingstudios/FlexingNetwork/api/util/ChatUtil.java
@@ -0,0 +1,34 @@
+package com.flexingstudios.FlexingNetwork.api.util;
+
+import net.md_5.bungee.api.chat.*;
+import org.jetbrains.annotations.NotNull;
+
+public class ChatUtil {
+    
+    private ChatUtil() {
+        throw new UnsupportedOperationException("This class cannot be instantiated");
+    }
+
+    public static BaseComponent createMessage(@NotNull String message, ClickEvent clickEvent, HoverEvent hoverEvent, String append, ComponentBuilder.FormatRetention format) {
+        TextComponent element = new TextComponent(new ComponentBuilder(message)
+                .event(clickEvent)
+                .event(hoverEvent)
+                .append(append, format)
+                .create());
+
+        return element;
+    }
+
+    public static BaseComponent createMessage(@NotNull String message) {
+        return new TextComponent(new ComponentBuilder(message).create());
+    }
+
+    public static BaseComponent createMessage(@NotNull String message, ClickEvent clickEvent, HoverEvent hoverEvent) {
+        TextComponent element = new TextComponent(new ComponentBuilder(message)
+                .event(clickEvent)
+                .event(hoverEvent)
+                .create());
+
+        return element;
+    }
+}
