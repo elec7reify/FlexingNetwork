28878ec11fb9a9c26e61dc76a9d2bff456e43aed
Vladislav <butnyk.vladislav@gmail.com>
2023-03-31 18:52:46 +0200

    Удалена интеграция локализации в Restart, изменены Messages

--- src/main/java/com/flexingstudios/FlexingNetwork/api/Language/Messages.java
+++ src/main/java/com/flexingstudios/FlexingNetwork/api/Language/Messages.java
@@ -1,76 +1,61 @@
 package com.flexingstudios.FlexingNetwork.api.Language;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Messages {
 
     /**  */
-    public static final String LANGUAGE_NAME = "name";
     public static final String NEW = "НОВОЕ";
-    public static final String COMMING_SOON = "СКОРО ПОЯВИТСЯ";
-    public static final String MENU_LANGUAGE_LORE = "language-menu-lore";
-    public static final String MENU_LANGUAGE_LORE_COMMING_SOON = "language-menu-lore." + COMMING_SOON;
-    public static final String MENU_LANGUAGE_TITLE = "language-menu-title";
-    public static final String PROFILE_MENU_TITLE = "profile-menu-title";
-    public static final String PLAYER_NOT_FOUND = "player-not-found";
-    public static final String ARROWTRAIL_NAME = "arrowtrail-";
-    public static final String ARROWTRAIL_HEARTS = "arrowtrail-hearts";
-    public static final String ARROWTRAIL_DRIP_WATER = "arrowtrail-drip_water";
-    public static final String ARROWTRAIL_DRIP_LAVA = "arrowtrail-drip_lava";
-    public static final String ARROWTRAIL_FIREWORK = "arrowtrail-firework";
-    public static final String ARROWTRAIL_NOTE = "arrowtrail-note";
-    public static final String ARROWTRAIL_SLIME = "arrowtrail-slime";
-    public static final String ARROWTRAIL_VILLAGER_HAPPY = "arrowtrail-villager_happy";
-    public static final String ARROWTRAIL_ANGRY_VILLAGER = "arrowtrail-angry_villager";
-    public static final String ARROWTRAIL_ENCHANTMENT_TABLE = "arrowtrail-enchantment_table";
-    public static final String ARROWTRAIL_SELECTED = "arrowtrail-selected";
-    public static final String CLICK_TO_BUY = "click-to-buy";
+    public static final String PROFILE_MENU_TITLE = "Профиль: {name}";
+    public static final String PLAYER_NOT_FOUND = "&cИгрок {player} не найден";
+    public static final String ARROWTRAIL_SELECTED = "&fВы выбрали &6след от стрелы&f: &a{trail_name}";
 
     /** BAN/KICK/MUTE Messages + SHADE */
-    public static final String REASON_NOT_SPECIFIED = "reason-not-specified";
-    public static final String COMMAND_KICK_USAGE = "cmd-kick-usage";
-    public static final String COMMAND_BAN_USAGE = "cmd-ban-usage";
-    public static final String KICKED_BY_SHADEADMIN = "kicked-by-shadeadmin";
-    public static final String KICKED_BY_ADMIN = "kicked-by-admin";
-    public static final String BANNED_BY_ADMIN = "banned-by-admin";
-    public static final String TEMPBANNED_BY_ADMIN = "tempbanned-by-admin";
-    public static final String KICK_MESSAGE = "kick-message";
-    public static final String BAN_MESSAGE = "ban-message";
+    public static final String REASON_NOT_SPECIFIED = "Не указана";
+    public static final String COMMAND_KICK_USAGE = "&cИспользование: /{command} <игрок> [причина]";
+    public static final String COMMAND_BAN_USAGE = "&cИспользование: /{command} <игрок> [время] [причина]";
+    public static final String KICKED_BY_SHADEADMIN = "&c&lТеневой админ &fкикнул игрока &3{player} &fпо причине: &6{reason}";
+    public static final String KICKED_BY_ADMIN = "&3{admin} &fкикнул игрока &3{player} &fпо причине: &6{reason}";
+    public static final String BANNED_BY_ADMIN = "&3{admin} &fзабанил игрока &3{player} &6навсегда &fпо причине: &6{reason}";
+    public static final String TEMPBANNED_BY_ADMIN = "&3{admin} &fзабанил игрока &3{player} &3на {time} &fпо причине: &6{reason}";
     public static final String BAN_ME = "&cВы не можете заблокировать самого себя!";
     public static final String KICK_ME = "&cВы не можете кикнуть самого себя!";
-    public static final String OFFICIAL_DATE_FORMAT = "date-format";
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
 
     /** Gamemode Command Messages */
-    public static final String COMMAND_GAMEMODE_USAGE = "cmd-gamemode-usage";
-    public static final String COMMAND_GAMEMODE_CHANGED = "cmd-gamemode-changed";
-    public static final String COMMAND_GAMEMODE_ERROR = "cmd-gamemode-error";
+    public static final String COMMAND_GAMEMODE_USAGE = "&cИспользование: /{command} <режим>";
+    public static final String COMMAND_GAMEMODE_CHANGED = "Ваш режим игры изменён на &a{mode}";
+    public static final String COMMAND_GAMEMODE_ERROR = "&cВаш режим игры уже является {mode}";
 
     /** Lobby Command Messages */
-    public static final String COMMAND_LOBBY_SUCCESSFUL = "cmd-lobby-successful";
-    public static final String COMMAND_LOBBY_ERROR = "cmd-lobby-error";
+    public static final String COMMAND_LOBBY_SUCCESSFUL = "&cВы были перенаправленны в лобби";
+    public static final String COMMAND_LOBBY_ERROR = "&aВы уже находитесь в лобби.";
 
     /** Ignore/Unignore Command Messages */
-    public static final String COMMAND_IGNORE_USAGE = "cmd-ignore-usage";
-    public static final String IGNORE_ALREADY = "ignore-already";
-    public static final String COMMAND_UNIGNORE_USAGE = "cmd-unignore-usage";
+    public static final String COMMAND_IGNORE_USAGE = "&cИспользование: /ignore <ник>";
+    public static final String IGNORE_ALREADY = "&cВы уже игнорируете этого игрока";
+    public static final String COMMAND_UNIGNORE_USAGE = "&cИспользование: /unignore <ник>";
 
     /** Restart task */
-    public static final String RESTART_BROADCAST = "restart-broadcast";
-    public static final String RESTART_BROADCAST_SCHEDULED = "restart-broadcast-scheduled";
-    public static final String RESTART_TIME = "restart-time-";
-    public static final String RESTART_KICK_REASON = "restart-kick-reason";
+    public static final List<String> RESTART_BROADCAST = Arrays.asList(
+            "&b------------------------------",
+            "&fСервер будет перезагружен через &35 минут&f!",
+            "&b------------------------------");
+    public static final String RESTART_BROADCAST_SCHEDULED = "&fСервер будет перезагружен через &3%time%&f!";
+    public static final String RESTART_KICK_REASON = "&cПерезагрузка сервера";
 
     /** Permission & Status */
-    public static final String NO_PERMISSION = "no-permission";
-    public static final String NO_RANK = "no-rank";
-
-    /** Donate Inventory */
-    public static final String CLOSE_DONATE_INVENTORY = "inventory-donate-close";
-    public static final String RANK_VIP_MENU = "inventory-rank-vip";
-    public static final String RANK_SPONSOR_MENU = "inventory-rank-sponsor";
-
-    public static final String RANK_VIP_LORE_SURVIVAL = RANK_VIP_MENU + ".lore";
-    public static final String RANK_SPONSOR_TITLE = RANK_SPONSOR_MENU + ".title";
-    public static final String RANK_SPONSOR_LORE_SURVIVAL = RANK_SPONSOR_MENU + ".lore";
+    public static final String NO_PERMISSION = "&cОтказано в доступе: у вас недостаточно прав для выполнения этого действия";
+    public static final String NO_RANK = "&cОтказано в доступе: для этого действия необходим ранг {rank}";
 
     /** ONLY Console AND ONLY in ENGLISH */
     public static final String CONSOLE_COMMAND_GAMEMODE_USAGE = "console-cmd-gamemode-usage";
+
+//                "&7После применения: язык",
+//                "&7интерфейса сервера будет",
+//                "&7изменён на Русский.",
+//                "",
+//                "&aНажмите, чтобы изменить язык",
+//                "&aна Русский."
 }
--- src/main/java/com/flexingstudios/FlexingNetwork/tasks/Restart.java
+++ src/main/java/com/flexingstudios/FlexingNetwork/tasks/Restart.java
@@ -5,35 +5,33 @@ import com.flexingstudios.FlexingNetwork.api.FlexingNetwork;
 import com.flexingstudios.FlexingNetwork.api.Language.Messages;
 import com.flexingstudios.FlexingNetwork.api.ServerType;
 import com.flexingstudios.FlexingNetwork.api.event.ServerRestartEvent;
-import com.flexingstudios.FlexingNetwork.api.player.Language;
 import com.flexingstudios.FlexingNetwork.api.util.Utilities;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public class Restart {
-    private static final int RESTART_HOUR = 48;
+    private static final int RESTART_HOUR = 2;
     private static boolean forced = false;
     private static ScheduledFuture<?> future;
 
     public static void schedule() {
         LocalDateTime now = LocalDateTime.now(FlexingNetwork.TZ_MOSCOW);
         int hours = now.getHour();
-        int waitHours = (hours >= 3) ? (23 - hours + 3) : (3 - hours - 1);
-        int waitMins= 59 - now.getMinute();
+        int waitHours = (hours >= RESTART_HOUR) ? (23 - hours + RESTART_HOUR) : (RESTART_HOUR - hours - 1);
+        int waitMins = 59 - now.getMinute();
         int waitSeconds = 59 - now.getSecond();
         if (waitHours < 2)
             waitHours += 24;
         long waitTimeMillis = ((waitHours * 3600 + waitMins * 60 + waitSeconds - 300) * 1000);
         if (future != null)
             future.cancel(false);
-        future = (FlexingNetworkPlugin.getInstance()).scheduledExecutorService.schedule(Restart::countdown, waitTimeMillis, TimeUnit.MILLISECONDS);
+        future = FlexingNetworkPlugin.getInstance().scheduledExecutorService.schedule(Restart::countdown, waitTimeMillis, TimeUnit.MILLISECONDS);
         Bukkit.getPluginManager().callEvent(new ServerRestartEvent(ServerRestartEvent.State.SCHEDULED, false));
     }
 
@@ -44,27 +42,20 @@ public class Restart {
         }
 
         Bukkit.getPluginManager().callEvent(new ServerRestartEvent(ServerRestartEvent.State.COUNTDOWN, forced));
-        Bukkit.getScheduler().scheduleSyncDelayedTask(FlexingNetworkPlugin.getInstance(), () -> {
-            for (Player player : Bukkit.getOnlinePlayers())
-                Utilities.msg(player, Language.getList(player, Messages.RESTART_BROADCAST));
-            Logger.getGlobal().info("The Server will restart in 5 minutes!");
-        });
+        Bukkit.getScheduler().scheduleSyncDelayedTask(FlexingNetworkPlugin.getInstance(), () -> Utilities.bcast(Messages.RESTART_BROADCAST));
 
         ScheduledExecutorService executor = FlexingNetworkPlugin.getInstance().scheduledExecutorService;
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "4min"), Language.getLang("en").getString(Messages.RESTART_TIME + "4min")), 60L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "3min"), Language.getLang("en").getString(Messages.RESTART_TIME + "3min")), 120L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "2min"), Language.getLang("en").getString(Messages.RESTART_TIME + "2min")), 180L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "1min"), Language.getLang("en").getString(Messages.RESTART_TIME + "1min")), 240L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "30sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "30sec")), 270L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "10sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "10sec")), 290L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "5sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "5sec")), 295L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "4sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "4sec")), 296L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "3sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "3sec")), 297L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "2sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "2sec")), 298L, TimeUnit.SECONDS);
-            executor.schedule(() -> bcast(Language.getMsg(player, Messages.RESTART_TIME + "1sec"), Language.getLang("en").getString(Messages.RESTART_TIME + "1sec")), 299L, TimeUnit.SECONDS);
-        }
-
+        executor.schedule(() -> broadcast("4 минуты"), 60L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("3 минуты"), 120L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("2 минуты"), 180L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("1 минуту"), 240L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("30 секунд"), 270L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("10 секунд"), 290L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("5 секунд"), 295L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("4 секунды"), 296L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("3 секунды"), 297L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("2 секунды"), 298L, TimeUnit.SECONDS);
+        executor.schedule(() -> broadcast("1 секунду"), 299L, TimeUnit.SECONDS);
         executor.schedule(() -> Bukkit.getScheduler().scheduleSyncDelayedTask(FlexingNetworkPlugin.getInstance(), Restart::doRestart), 300L, TimeUnit.SECONDS);
     }
 
@@ -73,10 +64,12 @@ public class Restart {
         doRestart();
     }
 
-    private static void bcast(String time, String consoleTime) {
-        for (Player player : Bukkit.getOnlinePlayers())
-            Utilities.msg(player, Language.getMsg(player, Messages.RESTART_BROADCAST_SCHEDULED).replace("{time}", time));
-//        Logger.getGlobal().info("The Server will restart in " + consoleTime +"!");
+    /**
+     * Notify the player how much time is left until the restart.
+     * @param time time left before restart. String Format: "0 seconds"
+     */
+    private static void broadcast(String time) {
+        Utilities.bcast(Messages.RESTART_BROADCAST_SCHEDULED.replace("%time%", time));
     }
 
     private static void doRestart() {
@@ -85,13 +78,13 @@ public class Restart {
         Bukkit.getPluginManager().callEvent(new ServerRestartEvent(ServerRestartEvent.State.RESTART, forced));
         if (FlexingNetwork.lobby().getServerType() == ServerType.LOBBY) {
             for (Player player : Bukkit.getOnlinePlayers())
-                player.kickPlayer(Utilities.colored(Language.getMsg(player, Messages.RESTART_KICK_REASON)));
+                player.kickPlayer(Utilities.colored(Messages.RESTART_KICK_REASON));
         } else if (forced) {
             for (Player player : Bukkit.getOnlinePlayers())
                 FlexingNetwork.toLobby(player);
         } else {
             for (Player player : Bukkit.getOnlinePlayers())
-                player.kickPlayer(Utilities.colored(Language.getMsg(player, Messages.RESTART_KICK_REASON)));
+                player.kickPlayer(Utilities.colored(Messages.RESTART_KICK_REASON));
         }
         
         Bukkit.getScheduler().scheduleSyncDelayedTask(FlexingNetworkPlugin.getInstance(), Bukkit::shutdown, 5L);
