acd9cf8dd5aa08eca356ea1c5765f3a7972d5520
Vladislav <butnyk.vladislav@gmail.com>
2023-04-08 00:49:05 +0200

    Rename from IC to ClickType

--- src/main/java/com/flexingstudios/FlexingNetwork/api/util/ClickType.java
+++ src/main/java/com/flexingstudios/FlexingNetwork/api/util/ClickType.java
@@ -4,20 +4,20 @@ import com.flexingstudios.FlexingNetwork.api.event.ItemClickedEvent;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class IC {
+public class ClickType {
     public static boolean isRightClick(PlayerInteractEvent event) {
-        return (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK);
+        return event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK;
     }
 
     public static boolean isRightClick(ItemClickedEvent event) {
-        return (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK);
+        return event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK;
     }
 
     public static boolean isLeftClick(PlayerInteractEvent event) {
-        return (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK);
+        return event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK;
     }
 
     public static boolean isLeftClick(ItemClickedEvent event) {
-        return (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK);
+        return event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK;
     }
 }
